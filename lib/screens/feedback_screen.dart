ccc62761794429212aca2bca0d8be92fcc1233c0
7SH7 <kimseunghwan7777@gmail.com>
2025-11-27 03:19:38 +0900

    🔥 chore: Remove unused app_bar_widget import from feedback screen

--- lib/screens/feedback_screen.dart
+++ lib/screens/feedback_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import '../widgets/app_bar_widget.dart';
 import '../widgets/gradient_button.dart';
 
 class FeedbackScreen extends StatefulWidget {
